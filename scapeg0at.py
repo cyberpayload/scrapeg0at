8c3115d1aecb87f431d5cfb97d7cfc70dac18bbb
Will Helsel <will@cyberpayload.com>
2020-06-30 07:22:12 -0500

    removed rstr module and pprint variable

--- scapeg0at.py
+++ scapeg0at.py
@@ -2,11 +2,9 @@
 
 import requests
 import re
-import rstr
 import time
 #This program is to scrape the current Linkedin occupation of someone.
 
-pp = pprint.PrettyPrinter()
 URL = 'https://www.google.com/search?rlz=1C1CHBF_enUS900US900&biw=1101&bih=1039&sxsrf=ALeKk02nIIgzxBoZRZLRcaPXCNQWWEqW_A%3A1593459510560&ei=NkP6XsXlIY-4sQXYrouIBA&q=site%3Alinkedin.com+intitle%3A%22William+Helsel%22&oq=site%3Alinkedin.com+intitle%3A%22William+Helsel%22&gs_lcp=CgZwc3ktYWIQA1C5EVj7MmCFPWgAcAB4AIABmQGIAdUTkgEEOS4xNZgBAKABAaoBB2d3cy13aXo&sclient=psy-ab&ved=0ahUKEwjF2Nyg46fqAhUPXKwKHVjXAkEQ4dUDCAw&uact=5'
 googleResultsPage = requests.get(URL)
 GRP = googleResultsPage.content
